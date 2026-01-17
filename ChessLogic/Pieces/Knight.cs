7ace74b0ee1f589f3f5990f7f0e7cfa3ae52a291
gamal yahia <gamal.0523028@gmail.com>
2026-01-17 07:17:16 +0200

    add moves

--- ChessLogic/Pieces/Knight.cs
+++ ChessLogic/Pieces/Knight.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public class Knight : Piece
     {
@@ -23,5 +17,30 @@ namespace ChessLogic
             copy.HasMoved = HasMoved;
             return copy;
         }
+
+        private static IEnumerable<Position> PotentialToPositions(Position from)
+        {
+
+            foreach (Direction vDir in new Direction[] { Direction.North, Direction.South })
+            {
+                foreach(Direction hDir in new Direction[] {Direction.West, Direction.East })
+                {
+                    yield return from + 2 * vDir + hDir;
+                    yield return from + 2 * hDir + vDir;
+                }
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from).Where(pos => Board.IsInside(pos)
+                && (board.IsEmpty(pos) || board[pos].Color != Color));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositions(from , board).Select(to => new NormalMove(from , to));
+        }
+
     }
 }
